dfc17017deaace6a5a51b1843ed4bab0970e49d8
王伟杰 <wangweijie@ztgame.com>
2019-07-02 23:08:21 +0800

    使用 markdown 发送信息，限制每分钟发送 20 次

--- app.py
+++ app.py
@@ -1,18 +1,48 @@
-import os
+# -*- coding: UTF-8 -*-
+
 import json
 import requests
 
+import config
+
+from time import sleep
 from flask import Flask
 from flask import request
+from redis import Redis
+
 
 app = Flask(__name__)
+redis = Redis(host=config.redis_host, port=config.redis_port)
+
+
+def send_data(data):
+    for i in range(60):
+        index_value = redis.get(config.redis_key_index_name)
+        if not index_value:
+            index = 0
+        else:
+            index = int(index_value)
+        index = (index + 1) % config.send_limit
+        key_name = config.redis_key_pre + str(index)
+        last_send = redis.get(key_name)
+        if not last_send:
+            redis.set(config.redis_key_index_name, index, 120)
+            redis.set(key_name, '1', 60)
+            send_alert(data)
+            return "send sess"
+        else:
+            print(key_name, last_send, i)
+        sleep(5)
+    print('time out')
 
 
 @app.route('/', methods=['POST', 'GET'])
 def send():
     if request.method == 'POST':
-        post_data = request.get_data()
-        send_alert(bytes2json(post_data))
+        post_data_str = request.get_data()
+        post_data_json = bytes2json(post_data_str)
+        post_data = json2markdown(post_data_json)
+        send_data(post_data)
         return 'success'
     else:
         return 'weclome to use prometheus alertmanager dingtalk webhook server!'
@@ -23,23 +53,50 @@ def bytes2json(data_bytes):
     return json.loads(data)
 
 
-def send_alert(data):
-    token = os.getenv('ROBOT_TOKEN')
-    if not token:
-        print('you must set ROBOT_TOKEN env')
-        return
-    url = 'https://oapi.dingtalk.com/robot/send?access_token=%s' % token
-    send_data = {
-        "msgtype": "text",
-        "text": {
-            "content": data
-        }
+def json2markdown(data):
+    markdown = {
+        'title': data['commonLabels']['alertname']
     }
-    req = requests.post(url, json=send_data)
-    result = req.json()
-    if result['errcode'] != 0:
-        print('notify dingtalk error: %s' % result['errcode'])
+
+    text = '# {}\n'.format(markdown['title'])
+    status = str(data['status'])
+    text = text + '## 状态: {}\n'.format(status)
+
+    if 'commonAnnotations' in data:
+        annotation = ''
+        for key, value in data['commonAnnotations'].items():
+            annotation = annotation + '- {}: {}\n'.format(key, str(value))
+        text = text + '## 注释: \n{}\n'.format(annotation)
+
+    if 'commonLabels' in data:
+        labels = ''
+        lebel_ignore = ['endpoint', 'namespace', 'ZoneId', 'prometheus', 'alertname', 'pod', 'service']
+        for key, value in data['commonLabels'].items():
+            if key not in lebel_ignore:
+                labels = labels + '- {}: {}\n'.format(key, str(value))
+        text = text + '## 标签: \n{}'.format(labels)
+
+    markdown['text'] = text
+
+    postdata = {
+        'msgtype': 'markdown',
+        'markdown': markdown
+    }
+
+    return postdata
+
+
+def send_alert(data):
+    url = 'https://oapi.dingtalk.com/robot/send?access_token=%s' % config.dingtalk_token
+    headers = {'Content-Type': 'application/json;charset=utf-8'}
+    req = requests.post(url, json.dumps(data), headers=headers)
+    req = json.loads(req.content)
+    if req[u'errcode'] > 0:
+        print(req)
 
 
 if __name__ == '__main__':
+    if not config.dingtalk_token:
+        print('you must set DINGTALK_TOKEN env')
+        exit()
     app.run(host='0.0.0.0', port=5000)
---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+
+redis_host = os.getenv('REDIS_HOST')
+if not redis_host:
+    redis_host = 'localhost'
+
+redis_port = os.getenv('REDIS_HOST')
+if not redis_port:
+    redis_port = 6379
+
+send_limit = os.getenv('SEND_LIMIT')
+if send_limit:
+    send_limit = int(send_limit)
+else:
+    send_limit = 20
+
+dingtalk_token = os.getenv('DINGTALK_TOKEN')
+redis_key_pre = 'alert-dingtalk-' + dingtalk_token[-9: -1] + '-'
+redis_key_index_name = redis_key_pre + 'index'
